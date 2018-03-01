1a8b2b627e3f76b40b066bb3f505755db118af86
GuillaumeLam <guillaume.lam10100@gmail.com>
2018-03-01 14:48:28 -0500

    initial commit

---
+++ src/postalrate-calc.py
@@ -0,0 +1,1 @@
+# maybe have class parcel so that it can be imported into tests.py and tested
---
+++ src/postalrate-csv-pop.py
@@ -0,0 +1,7 @@
+# if postalrates.csv is not found, this script will be called
+
+# script should generate n number of letter-number-letter, then
+# will go through all combinations (where not same code with itself)
+# and will generate same rand limits for size for all three prority
+# but three diff rand rate per gram (can have base and simply add percentage
+# of rate for next higher tier)
